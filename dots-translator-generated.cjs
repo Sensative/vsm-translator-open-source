68121d3c116d0c24bd3dea179e0e98f19869247b
Lars Mats <lars.mats@sensative.com>
2023-03-27 12:35:30 +0200

    New version of radar

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -222,6 +222,27 @@ M output air_breath_voc_equivalent 152 0x98  0.01
             }, 
         
 
+            559699338: {
+                name: "dots,temp,radar-v5",
+                versions: "",
+                mapData: `M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalMinutes 160 0xa0  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 161 0xa1  1
+M input tempAlarmHighLevel 162 0xa2  1
+M output amplitude 144 0x90  1
+M output distance 145 0x91  0.01
+M output occupied 129 0x81  1
+M input amplitudeHysteresis 179 0xb3  1
+M input distanceHysteresis 163 0xa3  0.01
+M output batteryPercent 164 0xa4  1
+M output underVoltage 165 0xa5  1
+`,
+            }, 
+        
+
             664086314: {
                 name: "dots,temp,comfort,air",
                 versions: "R0",
@@ -290,27 +311,6 @@ M output numSatellites 164 0xa4  1
 M output bestSatellites 184 0xb8  1
 M output scanCount 185 0xb9  1
 M output gpsTime 186 0xba  1
-`,
-            }, 
-        
-
-            777944170: {
-                name: "dots,temp,radar-v5",
-                versions: "",
-                mapData: `M output temp 176 0xb0  0.01
-M output averageTemp 177 0xb1  0.01
-M input tempHysteresis 178 0xb2  0.01
-M input averageTempIntervalMinutes 160 0xa0  1
-M output tempAlarm 128 0x80  1
-M input tempAlarmLowLevel 161 0xa1  1
-M input tempAlarmHighLevel 162 0xa2  1
-M output amplitude 144 0x90  1
-M output distance 145 0x91  0.01
-M output occupied 129 0x81  1
-M input amplitudeHysteresis 179 0xb3  1
-M input distanceHysteresis 163 0xa3  0.01
-M output batteryPercent 164 0xa4  1
-M output underVoltage 165 0xa5  1
 `,
             }, 
         
