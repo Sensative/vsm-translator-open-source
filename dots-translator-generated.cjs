9e77466ff27eedc007eb9eab1c5389471456e79c
Lars Mats <lars.mats@sensative.com>
2023-06-13 14:58:19 +0200

    New version of lifefinder squad

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -2324,6 +2324,25 @@ M input alarmAck 164 0xa4  1
             }, 
         
 
+            3242661238: {
+                name: "Lifefinder-squad",
+                versions: "",
+                mapData: `M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalMinutes 160 0xa0  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 161 0xa1  1
+M input tempAlarmHighLevel 162 0xa2  1
+M output alarmTime 144 0x90  1
+M output buttonAlarm 129 0x81  1
+M output volts 179 0xb3  0.001
+M input maxAlarmMinutes 180 0xb4  1
+M input alarmAck 164 0xa4  1
+`,
+            }, 
+        
+
             3257466388: {
                 name: "dots,temp,sound,comfort,air-v2",
                 versions: "",
