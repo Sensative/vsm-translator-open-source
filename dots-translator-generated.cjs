2d9e4dc75afd7de8d9f6693edcec323bc4f54bb8
Lars Mats <lars.mats@sensative.com>
2023-10-08 15:12:05 +0200

    Added airport-int (interrupt driven airport app)

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -2706,6 +2706,31 @@ M input alarmAck 164 0xa4  1
             }, 
         
 
+            3040106327: {
+                name: "Airport-int",
+                versions: "",
+                mapData: `M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalHours 160 0xa0  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 161 0xa1  1
+M input tempAlarmHighLevel 162 0xa2  1
+M input motionThreshold_mm_s2 179 0xb3  1
+M input limitedScanChannels 184 0xb8  1
+M input fullScanChannels 185 0xb9  1
+M output motionCount 186 0xba  1
+M input quarterlyScanBudget 163 0xa3  1
+M input maxBudget 180 0xb4  1
+M input singleWifiScanAgain_minutes 164 0xa4  1
+M input minimumWifiCount 165 0xa5  1
+M input fullWifiScan_minutes 166 0xa6  1
+M input gpsScan_minutes 167 0xa7  1
+M input motionCountEnabled 168 0xa8  1
+`,
+            }, 
+        
+
             3086277881: {
                 name: "Linkcheck",
                 versions: "R7 R6 R5 R4 R2",
