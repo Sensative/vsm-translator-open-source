704ff5b6ecd6e349647228454b80c4f7cd09a0ff
Lars Mats <lars.mats@sensative.com>
2023-11-27 11:09:08 +0100

    New revisions of all comfort/air apps - added join on button press

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -1372,6 +1372,42 @@ M input alarmAck 164 0xa4  1
             }, 
         
 
+            915094797: {
+                name: "Square-air",
+                versions: "",
+                mapData: `M input roamNetworkCount 160 0xa0  1
+M output batteryPercent 161 0xa1  1
+M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalMinutes 162 0xa2  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 163 0xa3  1
+M input tempAlarmHighLevel 164 0xa4  1
+M output humidity 179 0xb3  0.01
+M output averageHumidity 144 0x90  0.01
+M input humidityTreshold 180 0xb4  0.01
+M input averageHumidityIntervalMinutes 165 0xa5  1
+M output lux 181 0xb5  1
+M output averageLux 145 0x91  1
+M input luxTresholdPercent 182 0xb6  1
+M input averageLuxIntervalMinutes 166 0xa6  1
+M output air_run_in_status 167 0xa7  1
+M output air_stab_status 168 0xa8  1
+M output air_iaq_accuracy 169 0xa9  1
+M output air_iaq 146 0x92  1
+M output air_co2 147 0x93  1
+M output air_pressure 184 0xb8  0.01
+M output air_breath_voc_equivalent 152 0x98  0.01
+M output air_static_iaq 185 0xb9  1
+M input air_interval_minutes 170 0xaa  1
+M input air_static_iaq_alarm_level 186 0xba  1
+M input air_iaq_alarm_level 187 0xbb  1
+M output air_iaq_alarm 129 0x81  1
+`,
+            }, 
+        
+
             924779957: {
                 name: "Puck-radar-v6",
                 versions: "R8 R7",
@@ -2750,6 +2786,34 @@ M output buttonCount 152 0x98  1
             }, 
         
 
+            2387046759: {
+                name: "Square-comfort-sound",
+                versions: "",
+                mapData: `M input roamNetworkCount 160 0xa0  1
+M output batteryPercent 161 0xa1  1
+M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalMinutes 162 0xa2  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 163 0xa3  1
+M input tempAlarmHighLevel 164 0xa4  1
+M output humidity 179 0xb3  0.01
+M output averageHumidity 144 0x90  0.01
+M input humidityTreshold 180 0xb4  0.01
+M input averageHumidityIntervalMinutes 165 0xa5  1
+M output lux 181 0xb5  1
+M output averageLux 145 0x91  1
+M input luxTresholdPercent 182 0xb6  1
+M input averageLuxIntervalMinutes 166 0xa6  1
+M output soundLevel 183 0xb7  0.1
+M input soundThreshold 167 0xa7  1
+M input soundMinLevel 168 0xa8  1
+M input soundAvgMinutes 169 0xa9  1
+`,
+            }, 
+        
+
             2391995788: {
                 name: "Square-comfort-sound",
                 versions: "R8 R7 R6 R5",
@@ -3651,6 +3715,30 @@ M input tempAlarmLowLevel 161 0xa1  1
 M input tempAlarmHighLevel 162 0xa2  1
 M output volts 179 0xb3  0.001
 M input motionThresholdG 180 0xb4  0.001
+`,
+            }, 
+        
+
+            3530880224: {
+                name: "Square-comfort",
+                versions: "",
+                mapData: `M input roamNetworkCount 160 0xa0  1
+M output batteryPercent 161 0xa1  1
+M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalMinutes 162 0xa2  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 163 0xa3  1
+M input tempAlarmHighLevel 164 0xa4  1
+M output humidity 179 0xb3  0.01
+M output averageHumidity 144 0x90  0.01
+M input humidityTreshold 180 0xb4  0.01
+M input averageHumidityIntervalMinutes 165 0xa5  1
+M output lux 181 0xb5  1
+M output averageLux 145 0x91  1
+M input luxTresholdPercent 182 0xb6  1
+M input averageLuxIntervalMinutes 166 0xa6  1
 `,
             }, 
         
