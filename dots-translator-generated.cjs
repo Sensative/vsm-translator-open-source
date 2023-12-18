2c48d14586dcb8f7961b4a29e52359a8ddc80f15
Lars Mats <lars.mats@sensative.com>
2023-12-18 10:42:01 +0100

    Updated airport app

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -2180,6 +2180,34 @@ M input motionThreshold 183 0xb7  0.001
             }, 
         
 
+            1695912328: {
+                name: "Airport-int",
+                versions: "",
+                mapData: `M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalHours 160 0xa0  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 161 0xa1  1
+M input tempAlarmHighLevel 162 0xa2  1
+M input motionThreshold_mm_s2 179 0xb3  1
+M input limitedScanChannels 184 0xb8  1
+M input fullScanChannels 185 0xb9  1
+M output motionCount 186 0xba  1
+M input quarterlyScanBudget 163 0xa3  1
+M input maxBudget 180 0xb4  1
+M input singleWifiScanAgain_minutes 164 0xa4  1
+M input minimumWifiCount 165 0xa5  1
+M input fullWifiScan_minutes 166 0xa6  1
+M input gpsScan_minutes 167 0xa7  1
+M input motionCountEnabled 168 0xa8  1
+M input abandonedCartTime_minutes 181 0xb5  1
+M output abandonedCart 169 0xa9  1
+M output batteryPercent 170 0xaa  1
+`,
+            }, 
+        
+
             1702715986: {
                 name: "dots,temp,lifefinder",
                 versions: "",
