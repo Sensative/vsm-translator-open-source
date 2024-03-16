1c91ecac70755a096f0b0459859856ae1bb39845
DP <dhiraj.paryani@sensative.com>
2024-03-16 11:17:37 +0100

    Added export knownSchemas statement to the template and generated dots-translator files.

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -4593,6 +4593,9 @@ M output underVoltage 165 0xa5 1
 
 /// END DO NOT CHANGE THE ABOVE
 
+// Export knownSchemas
+exports.knownSchemas = knownSchemas;
+
 // Response to a reference value query
     const decodeReferences = (iotnode, symbolTable, data, time) => {
         let result = {};
