d48ebd4dbe17327e3fab4a943451d6793a98a5de
DP <dhiraj.paryani@sensative.com>
2024-04-19 16:28:38 +0200

    export knownSchemas updated based on feedback from Lars Mats.

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -3870,7 +3870,10 @@ M output underVoltage 165 0xa5 1
 /// END DO NOT CHANGE THE ABOVE
 
 // Export knownSchemas
-exports.knownSchemas = knownSchemas;
+try {
+    exports.knownSchemas = knownSchemas;
+}
+catch (ignored) {}
 
 // Response to a reference value query
     const decodeReferences = (iotnode, symbolTable, data, time) => {
