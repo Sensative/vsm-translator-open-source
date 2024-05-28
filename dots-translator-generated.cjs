ad938fefaec7cbad6418fab2e2910a2dc47bfc5c
Lars Mats <lars.mats@sensative.com>
2024-05-28 09:34:29 +0200

    Changed air app to not do confirmed reporting as often

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -2493,6 +2493,42 @@ M input alarmAck 164 0xa4  1
             }, 
         
 
+            3241343967: {
+                name: "Square-air",
+                versions: "",
+                mapData: `M input roamNetworkCount 160 0xa0  1
+M output batteryPercent 161 0xa1  1
+M output temp 176 0xb0  0.01
+M output averageTemp 177 0xb1  0.01
+M input tempHysteresis 178 0xb2  0.01
+M input averageTempIntervalMinutes 162 0xa2  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 163 0xa3  1
+M input tempAlarmHighLevel 164 0xa4  1
+M output humidity 179 0xb3  0.01
+M output averageHumidity 144 0x90  0.01
+M input humidityTreshold 180 0xb4  0.01
+M input averageHumidityIntervalMinutes 165 0xa5  1
+M output lux 181 0xb5  1
+M output averageLux 145 0x91  1
+M input luxTresholdPercent 182 0xb6  1
+M input averageLuxIntervalMinutes 166 0xa6  1
+M output air_run_in_status 167 0xa7  1
+M output air_stab_status 168 0xa8  1
+M output air_iaq_accuracy 169 0xa9  1
+M output air_iaq 184 0xb8  1
+M output air_co2 185 0xb9  1
+M output air_pressure 186 0xba  0.01
+M output air_breath_voc_equivalent 187 0xbb  0.01
+M output air_static_iaq 188 0xbc  1
+M input air_interval_minutes 170 0xaa  1
+M input air_static_iaq_alarm_level 189 0xbd  1
+M input air_iaq_alarm_level 190 0xbe  1
+M output air_iaq_alarm 129 0x81  1
+`,
+            }, 
+        
+
             3264322911: {
                 name: "Square-comfort",
                 versions: "R9 R10",
@@ -3727,7 +3763,7 @@ M output underVoltage 165 0xa5 1
         idd.gnssSnrLpf = (data[b++]|data[b++]<<8)/10.0;
         idd.gnssSnrMaxLpf = (data[b++]|data[b++]<<8)/10.0;
         idd.loraRssiLpf = (0xffff0000|data[b++]|data[b++]<<8)/10.0;
-        idd.loraSnrLpf = (data[b++]|data[b++]<<8)/10.0;
+        idd.loraSnrLpf = ((data[b++]&0xff)|data[b++]<<8)/10.0;
         idd.wifiGwsLpf = data[b++]/10.0; 
         idd.gnssValidSVLpf = data[b++]/10.0; 
 
@@ -4314,7 +4350,7 @@ M output gnssState 160 0xa0 1
                         "gnssSnrLpf":0,
                         "gnssSnrMaxLpf":0,
                         "loraRssiLpf":-78,
-                        "loraSnrLpf":1475.3,
+                        "loraSnrLpf":1475.3, // Strange value, correction made 20240527
                         "wifiGwsLpf":0.3,
                         "gnssValidSVLpf":0.8}
                     }
