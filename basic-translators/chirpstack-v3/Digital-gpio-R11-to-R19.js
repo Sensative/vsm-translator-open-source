8d1b4fda90909c899ab52f85c66779538575a36b
Dhiraj Paryani <dhiraj.paryani@sensative.com>
2024-07-19 12:29:39 +0200

    Updated basic translators to reflect R19 release.

--- basic-translators/chirpstack-v3/Digital-gpio-R11-to-R19.js
+++ basic-translators/chirpstack-v3/Digital-gpio-R11-to-R19.js
@@ -42,7 +42,7 @@ function Decode(fPort, bytes, variables) {
             maxSize: 256
         },
         vsm: {
-            rulesCrc32: 3241343967 //Hardcoded - IT IS REPLACED AUTOMATICALLY WITH KNOWN SCHEMAS
+            rulesCrc32: 1617091456 //Hardcoded - IT IS REPLACED AUTOMATICALLY WITH KNOWN SCHEMAS
         }
     }
 
@@ -56,12 +56,14 @@ function Decode(fPort, bytes, variables) {
 function translate(iotnode) {
 
     /// DO NOT CHANGE THE BELOW - IT IS REPLACED AUTOMATICALLY WITH KNOWN SCHEMA
+
+    // CRCs having similar schema: 1617091456, 3497448490
     var schema = 
     {
-        3241343967: {
-            name: "Square-air",
-            versions: "R16 R18",
-            mapData: "M input roamNetworkCount 160 0xa0  1 + M output batteryPercent 161 0xa1  1 + M output temp 176 0xb0  0.01 + M output averageTemp 177 0xb1  0.01 + M input tempHysteresis 178 0xb2  0.01 + M input averageTempIntervalMinutes 162 0xa2  1 + M output tempAlarm 128 0x80  1 + M input tempAlarmLowLevel 163 0xa3  1 + M input tempAlarmHighLevel 164 0xa4  1 + M output humidity 179 0xb3  0.01 + M output averageHumidity 144 0x90  0.01 + M input humidityTreshold 180 0xb4  0.01 + M input averageHumidityIntervalMinutes 165 0xa5  1 + M output lux 181 0xb5  1 + M output averageLux 145 0x91  1 + M input luxTresholdPercent 182 0xb6  1 + M input averageLuxIntervalMinutes 166 0xa6  1 + M output air_run_in_status 167 0xa7  1 + M output air_stab_status 168 0xa8  1 + M output air_iaq_accuracy 169 0xa9  1 + M output air_iaq 184 0xb8  1 + M output air_co2 185 0xb9  1 + M output air_pressure 186 0xba  0.01 + M output air_breath_voc_equivalent 187 0xbb  0.01 + M output air_static_iaq 188 0xbc  1 + M input air_interval_minutes 170 0xaa  1 + M input air_static_iaq_alarm_level 189 0xbd  1 + M input air_iaq_alarm_level 190 0xbe  1 + M output air_iaq_alarm 129 0x81  1"
+        1617091456: {
+            name: "Digital-gpio",
+            versions: "R11 R12 R13 R14 R15 R16 R18 R19",
+            mapData: "M input roamNetworkCount 160 0xa0  1 + M output detection 128 0x80  1 + M input activation 129 0x81  1 + M output temp 176 0xb0  0.01 + M output averageTemp 177 0xb1  0.01 + M input tempHysteresis 178 0xb2  0.01 + M input averageTempIntervalHours 161 0xa1  1 + M output tempAlarm 130 0x82  1 + M input tempAlarmLowLevel 162 0xa2  1 + M input tempAlarmHighLevel 163 0xa3  1"
         }
     };
     /// END DO NOT CHANGE THE ABOVE 
