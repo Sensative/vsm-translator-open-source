15e9837047c9b6b1d9a9d858c272d3dd1cfa67a9
Dhiraj Paryani <dhiraj.paryani@sensative.com>
2024-10-25 16:50:08 +0200

    Updated for release R23.

--- basic-translators/chirpstack-v3/Tracker-R22-to-R23.js
+++ basic-translators/chirpstack-v3/Tracker-R22-to-R23.js
@@ -42,7 +42,7 @@ function Decode(fPort, bytes, variables) {
             maxSize: 256
         },
         vsm: {
-            rulesCrc32: 1815337626 //Hardcoded - IT IS REPLACED AUTOMATICALLY WITH KNOWN SCHEMAS
+            rulesCrc32: 2029355788 //Hardcoded - IT IS REPLACED AUTOMATICALLY WITH KNOWN SCHEMAS
         }
     }
 
@@ -58,10 +58,10 @@ function translate(iotnode) {
     /// DO NOT CHANGE THE BELOW - IT IS REPLACED AUTOMATICALLY WITH KNOWN SCHEMA
     var schema = 
     {
-        1815337626: {
-            name: "Gnss-autonomous-test",
-            versions: "R11 R12 R13 R14 R15 R16 R18 R19 R20 R21",
-            mapData: "M output numSatellites 160 0xa0  1 + M output bestSatellites 184 0xb8  1 + M output scanCount 185 0xb9  1 + M output batteryPercent 161 0xa1  1"
+        2029355788: {
+            name: "Tracker",
+            versions: "R22 R23",
+            mapData: "M output temp 176 0xb0  0.01 + M output averageTemp 177 0xb1  0.01 + M input tempHysteresis 178 0xb2  0.01 + M input averageTempIntervalHours 160 0xa0  1 + M output tempAlarm 128 0x80  1 + M input tempAlarmLowLevel 161 0xa1  1 + M input tempAlarmHighLevel 162 0xa2  1 + M input roamNetworkCount 163 0xa3  1 + M input stillMotionThreshold_mm_s2 179 0xb3  1 + M input movingMotionThreshold_mm_s2 180 0xb4  1 + M input limitedScanChannels 184 0xb8  1 + M input fullScanChannels 185 0xb9  1 + M output motionCount 186 0xba  1 + M output motionTimeMinutes 187 0xbb  1 + M input quarterlyScanBudget 164 0xa4  1 + M input maxBudget 181 0xb5  1 + M input quickRejoinBudgetMax 182 0xb6  1 + M input singleWifiScanAgain_minutes 165 0xa5  1 + M input minimumWifiCount 166 0xa6  1 + M input minimumGnssCount 167 0xa7  1 + M input fullWifiScan_minutes 168 0xa8  1 + M input gpsScan_minutes 169 0xa9  1 + M input motionCountEnabled 170 0xaa  1 + M input motionTimeEnabled 171 0xab  1 + M input gnssMode 172 0xac  1 + M input movingScanIntervalMinutes 188 0xbc  1 + M input stationaryScanIntervalMinutes 189 0xbd  1 + M input backUpWifiGnssScanMinutes 173 0xad  1 + M output batteryPercent 174 0xae  1"
         }
     };
     /// END DO NOT CHANGE THE ABOVE 
@@ -220,7 +220,7 @@ function translate(iotnode) {
             console.log("Unknown application with CRC32: " + rulesCrc32);
         }
 
-        var translatorVersion = "0.2.71"; // Replaced when creating new CRC based basic translators
+        var translatorVersion = "0.2.80"; // Replaced when creating new CRC based basic translators
         if (data.length < 8) {
             var resultVsm = {}; // This new object will hold the combined properties.
 
