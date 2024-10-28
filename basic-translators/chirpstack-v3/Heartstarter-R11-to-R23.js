0010222b591c267da948073370962eaabe82a3f8
Dhiraj Paryani <dhiraj.paryani@sensative.com>
2024-10-28 17:58:11 +0100

    Regenerated basic translators based on the new requirement.

--- basic-translators/chirpstack-v3/Heartstarter-R11-to-R23.js
+++ basic-translators/chirpstack-v3/Heartstarter-R11-to-R23.js
@@ -61,7 +61,7 @@ function translate(iotnode) {
         1727723267: {
             name: "Heartstarter",
             versions: "R11 R12 R13 R14 R15 R16 R18 R19 R20 R21 R22 R23",
-            mapData: "M output temp 176 0xb0  0.01 + M output averageTemp 177 0xb1  0.01 + M input tempHysteresis 178 0xb2  0.01 + M input averageTempIntervalHours 160 0xa0  1 + M output tempAlarm 128 0x80  1 + M input tempAlarmLowLevel 161 0xa1  1 + M input tempAlarmHighLevel 162 0xa2  1 + M output buttonDetect 129 0x81  1 + M output motionDetect 130 0x82  1 + M input soundScanIntervalMinutes 179 0xb3  1 + M input soundScanMinimumRepeat 163 0xa3  1 + M output soundIntervalS 152 0x98  0.001 + M output soundRepeat 131 0x83  1 + M input motionThresholdG 180 0xb4  0.001 + M output volts 181 0xb5  0.001 + M output batteryPercent 164 0xa4  1"
+            mapData: "M input averageTempIntervalHours 160 0xa0  1 + M input motionThresholdG 180 0xb4  0.001 + M input soundScanIntervalMinutes 179 0xb3  1 + M input soundScanMinimumRepeat 163 0xa3  1 + M input tempAlarmHighLevel 162 0xa2  1 + M input tempAlarmLowLevel 161 0xa1  1 + M input tempHysteresis 178 0xb2  0.01 + M output averageTemp 177 0xb1  0.01 + M output batteryPercent 164 0xa4  1 + M output buttonDetect 129 0x81  1 + M output motionDetect 130 0x82  1 + M output soundIntervalS 152 0x98  0.001 + M output soundRepeat 131 0x83  1 + M output temp 176 0xb0  0.01 + M output tempAlarm 128 0x80  1 + M output volts 181 0xb5  0.001"
         }
     };
     /// END DO NOT CHANGE THE ABOVE 
