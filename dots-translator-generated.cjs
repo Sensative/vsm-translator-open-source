0613d986ae069aeeb121d5965e40942346c2bccc
Lars Mats <lars.mats@sensative.com>
2025-01-09 16:16:28 +0100

    WIP: Integrating updated mesh applications

--- dots-translator-generated.cjs
+++ dots-translator-generated.cjs
@@ -2055,32 +2055,6 @@ M output buttonCount 152 0x98  1
             }, 
         
 
-            2016449019: {
-                name: "MeshComfortTimeCount",
-                versions: "",
-                mapData: `M input meshSyncInterval_h 160 0xa0  1
-M input meshEnableExtension 161 0xa1  1
-M output minutes 162 0xa2  1
-M output hours 163 0xa3  1
-M output days 164 0xa4  1
-M output weeks 176 0xb0  1
-M input interval_minutes 165 0xa5  1
-M output temp 177 0xb1  0.01
-M output averageTemp 178 0xb2  0.01
-M input tempHysteresis 179 0xb3  0.01
-M input averageTempIntervalMinutes 166 0xa6  1
-M output tempAlarm 128 0x80  1
-M input tempAlarmLowLevel 167 0xa7  1
-M input tempAlarmHighLevel 168 0xa8  1
-M output humidity 180 0xb4  0.01
-M output averageHumidity 144 0x90  0.01
-M input humidityTreshold 181 0xb5  0.01
-M input averageHumidityIntervalMinutes 169 0xa9  1
-M output batteryPercent 170 0xaa  1
-`,
-            }, 
-        
-
             2029355788: {
                 name: "Tracker",
                 versions: "R25 R24 R23 R22",
@@ -3906,6 +3880,32 @@ M input amplitudeHysteresis 179 0xb3  1
 M input distanceHysteresis 164 0xa4  0.01
 M output batteryPercent 165 0xa5  1
 M output underVoltage 166 0xa6  1
+`,
+            }, 
+        
+
+            4084549093: {
+                name: "MeshComfortTimeCount",
+                versions: "",
+                mapData: `M input meshSyncInterval_h 160 0xa0  1
+M input meshEnableExtension 161 0xa1  1
+M output minutes 162 0xa2  1
+M output hours 163 0xa3  1
+M output days 164 0xa4  1
+M output weeks 176 0xb0  1
+M input interval_minutes 165 0xa5  1
+M output temp 177 0xb1  0.01
+M output averageTemp 178 0xb2  0.01
+M input tempHysteresis 179 0xb3  0.01
+M input averageTempIntervalMinutes 166 0xa6  1
+M output tempAlarm 128 0x80  1
+M input tempAlarmLowLevel 167 0xa7  1
+M input tempAlarmHighLevel 168 0xa8  1
+M output humidity 180 0xb4  0.01
+M output averageHumidity 144 0x90  0.01
+M input humidityTreshold 181 0xb5  0.01
+M input averageHumidityIntervalMinutes 169 0xa9  1
+M output batteryPercent 170 0xaa  1
 `,
             }, 
         
