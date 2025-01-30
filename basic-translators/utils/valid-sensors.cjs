dc08731df357683b8fbd273aa4572c2459ed98a9
Dhiraj Paryani <dhiraj.paryani@sensative.com>
2025-01-30 20:11:58 +0100

    Added motion-spectrum sensor + some refactoring.

---
+++ basic-translators/utils/valid-sensors.cjs
@@ -0,0 +1,15 @@
+// List of allowed Sensative sensors and devices
+const validSensors = [
+    "Digital-gpio",             // Digital General Purpose Input/Output
+    "Lifefinder-alternating",   // Lifefinder sensor with alternating signal
+    "Lifefinder-gnss",          // Lifefinder sensor with GNSS (Global Navigation Satellite System)
+    "Lifefinder-wifi",          // Lifefinder sensor with WiFi capability
+    "Motion-measure",           // Motion measurement sensor
+    "Motion-spectrum",          // Motion spectrum sensor
+    "Puck-radar",               // Puck-shaped radar sensor
+    "Square-air",               // Square-shaped air quality sensor
+    "Square-comfort",           // Square-shaped comfort sensor
+    "Square-comfort-sound",     // Square-shaped comfort sensor with sound measurement
+    "Square-sound",             // Square-shaped sound sensor
+    "Tracker"                   // Tracker device
+];
