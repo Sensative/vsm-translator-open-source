7fd7beafaab302a5ef7d09b007ad5958f3d9f300
Dhiraj Paryani <dhiraj.paryani@sensative.com>
2025-04-08 17:07:48 +0200

    [Templates] Replaced KnownSchemas with schema.

--- basic-translators/chirpstack-v3/template/dots-chirpstack-v3-decoder-template.cjs
+++ basic-translators/chirpstack-v3/template/dots-chirpstack-v3-decoder-template.cjs
@@ -264,7 +264,7 @@ function translate(iotnode) {
                 schema: schema[rulesCrc32].mapData,
                 appVersions: schema[rulesCrc32].versions,
                 // Added as requirement from product owner:
-                appDocumentationUrl: "https://github.com/Sensative/vsm-application-documentation/blob/master/" + rulesCrc32 + "-" + knownSchemas[rulesCrc32].name + ".vso.md",
+                appDocumentationUrl: "https://github.com/Sensative/vsm-application-documentation/blob/master/" + rulesCrc32 + "-" + schema[rulesCrc32].name + ".vso.md",
                 appConfigurationUrl: "https://vsm-lora-config-app.service.sensative.net/?app=" + rulesCrc32,
                 // Good to know how old this information is
                 timestamp: new Date().toISOString(),
